28d899a2a1bc8697ae8bcc9ce425adb2321a9bb3
Dmitriy-Fedorov <d.d.96@yandex.ru>
2023-09-01 02:42:27 +0600

    Update link

--- oh-my-posh.lua
+++ oh-my-posh.lua
@@ -1,1 +1,1 @@
-load(io.popen('oh-my-posh init cmd --config C:/Users/DRND/negligible.omp.json'):read("*a"))()
+load(io.popen('oh-my-posh init cmd --config https://raw.githubusercontent.com/Dmitriy-Fedorov/Oh-my-posh-theme/main/negligible.omp.mod.json'):read("*a"))()
